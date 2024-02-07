ba31a8e701f7fefa3e59d8804dc2af2541ddd6ef
Lucas Trindade <trindadebra@gmail.com>
2024-02-06 21:45:34 -0300

    merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,36 @@ import { useEffect, useState } from 'react';
 const Home: React.FC = () => {
   const [botStatus, setBotStatus] = useState<string | null>(null);
 
-  // const [logs, setLogs] = useState<string[]>([]);
-
   const runBot = async () => {
     try {
-      const response = await fetch('/api/roadmaps/test');
+      const response = await fetch('/api/functions/clickInitial');
       const data = await response.json();
 
       setBotStatus(data.message);
-      // setLogs(data.logs);
+
+      // Se houver logs, você pode exibi-los em tempo real, se necessário
+      if (data.logs) {
+        data.logs.forEach((log: string) => {
+          console.log("Log:", log);
+        });
+      }
+
     } catch (error) {
       console.error(error);
       setBotStatus('Erro ao executar o bot.');
     }
   };
 
-  // useEffect(() => {
-  //   // Aqui você pode processar e exibir os logs conforme necessário
-  //   console.log("Logs:", logs);
-  // }, [logs]);
+  // Função para fazer polling a cada 5 segundos (ou outro intervalo desejado)
+  const startPolling = () => {
+    setInterval(() => {
+      runBot();
+    }, 500); // 5000 milissegundos = 5 segundos
+  };
+
+  useEffect(() => {
+    startPolling();
+  }, []); // Executa uma vez quando o componente é montado
 
   return (
     <div>
@@ -34,4 +45,3 @@ const Home: React.FC = () => {
 };
 
 export default Home;
-
